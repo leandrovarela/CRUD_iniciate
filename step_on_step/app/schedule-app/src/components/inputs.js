b5caa9cd2c0c063357a10158479d359a8f9e4c75
leandrovarela94 <leandro_varela@live.com>
2022-11-30 02:15:18 -0300

    V11 -Iniciate v.1

--- step_on_step/app/schedule-app/src/components/inputs.js
+++ step_on_step/app/schedule-app/src/components/inputs.js
@@ -3,98 +3,82 @@ import EditIcon from "@mui/icons-material/Edit";
 import { Button, TextField } from "@mui/material";
 import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
 import React, { useState } from "react";
-import contacts from "../database/db";
-
-const columns = [
-  { field: "name", headerName: "Name", width: 400, editable: true },
-  { field: "email", headerName: "Email", width: 300, editable: true },
-  {
-    field: "phone",
-    headerName: "Phone",
-    width: 200,
-    type: "String",
-    editable: true,
-  },
-
-  {
-    field: "edit",
-    headerName: "Edit",
-    type: "actions",
-    width: 100,
-    getActions: (row) => [
-      <GridActionsCellItem
-        //onClick={updateContact(row.id)}
-        icon={<EditIcon />}
-        label="Edit"
-      />,
-    ],
-  },
-  {
-    field: "delete",
-    headerName: "Delete",
-    type: "actions",
-    width: 100,
-
-    getActions: (row) => [
-      <GridActionsCellItem
-        onClick={() => deleteContact(row.id)}
-        icon={<DeleteIcon />}
-        label="Delete"
-      />,
-    ],
-  },
-];
 
 const ContactRenders = () => {
   // Hooks for getInputs
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
-  const [rows, setRows] = useState(contacts);
+  const [contact, setContact] = useState([]);
+  const id = Date.now() * Math.random();
 
-  //Create Function Delete
-  const deleteContact = (id) => {
-    setRows((prevRows) => prevRows.filter((rows) => rows.id !== id));
+  const getContacts = () => {
+    fetch("http://localhost:5000/contacts/", {
+      method: "GET",
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setContact(data);
+        console.log(data);
+      });
   };
 
-  //Create Funtion Update
-
-  //const updateContact = useCallback(
-  // (id) => () => {
-  // setRows((oldRows) =>
-  //   oldRows.filter((array) => array.id.indexOf("id") !== -1)
-  // );
-  // console.log(setRows(id));
-  //},
-  //[]
-  //);
-
-  // Function for Create Contacts
   const createContact = () => {
-    const newContact = { id: Date.now() * Math.random(), name, phone, email };
-    const contact_temp = [...rows];
-
-    contact_temp.push(newContact);
-
-    console.log(newContact);
-    if (typeof newContact !== "undefined") setRows(contact_temp);
+    fetch("http://localhost:5000/contacts/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: id,
+        name: name,
+        phone: phone,
+        email: email,
+      }),
+    });
   };
 
-  const handleSubmit = () => {
-    createContact();
-  };
+  const handleSubmit = () => createContact();
 
-  // fetch("http://localhost:5000/contacts/", {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  // })
-  //   .then((resp) => resp.json())
-  //   .then((data) => {
-  //     id, name(data), phone(data), email(data);
-  //   })
-  //   .catch((err) => console.log(err));
+  const columns = [
+    { field: "name", headerName: "Name", width: 400, editable: true },
+    { field: "email", headerName: "Email", width: 300, editable: true },
+    {
+      field: "phone",
+      headerName: "Phone",
+      width: 200,
+      type: "String",
+      editable: true,
+    },
+
+    {
+      field: "edit",
+      headerName: "Edit",
+      type: "actions",
+      width: 100,
+      getActions: (row) => [
+        <GridActionsCellItem
+          //onClick={updateContact(row.id)}
+          icon={<EditIcon />}
+          label="Edit"
+        />,
+      ],
+    },
+    {
+      field: "delete",
+      headerName: "Delete",
+      type: "actions",
+      width: 100,
+
+      getActions: (contact) => [
+        <GridActionsCellItem
+          //onClick={() => deleteContact(contact.id)}
+          icon={<DeleteIcon />}
+          label="Delete"
+        />,
+      ],
+    },
+  ];
 
   return (
     <>
@@ -148,11 +132,12 @@ const ContactRenders = () => {
       </div>
       <div style={{ height: 400, width: "100%" }}>
         <DataGrid
-          rows={rows}
+          rows={contact}
           columns={columns}
           pageSize={5}
           rowsPerPageOptions={[1]}
         />
+        <Button onClick={() => getContacts()}>pega tudo</Button>
       </div>
     </>
   );
