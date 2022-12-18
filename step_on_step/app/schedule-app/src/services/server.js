99ca158ba75bf380b82311df5eb4672cd763673e
leandrovarela94 <leandro_varela@live.com>
2022-12-18 11:58:28 -0300

    V12 - SEMI COMPLETE

--- step_on_step/app/schedule-app/src/services/server.js
+++ step_on_step/app/schedule-app/src/services/server.js
@@ -11,51 +11,57 @@ const contacts = [
     phone: "+55(21) 97477 - 0964",
     email: "varela@gmail.com",
   },
-  {
-    id: 2,
-    name: "LUCAS VARELA PEREIRA",
-    phone: "+55(21) 97477 - 0964",
-    email: "lucas@gmail.com",
-  },
-  {
-    id: 3,
-    name: "ROBERT VARELA PEREIRA",
-    phone: "+55(21) 97477 - 0964",
-    email: "robert@gmail.com",
-  },
-  {
-    id: 4,
-    name: "JUNIOR VARELA PEREIRA",
-    phone: "+55(21) 97477 - 0964",
-    email: "melynx@gmail.com",
-  },
 ];
 
 server.get("/contacts", (req, res) => {
   return res.json(contacts);
 });
 
+server.get("/contacts/:id", (req, res) => {
+  const { id } = request.params;
+
+  const contact = contacts.find((contact) => contact.id === id);
+  console.log(contact);
+
+  return res.json(contact);
+});
+
 server.post("/contacts", (req, res) => {
-  const contact = req.body;
+  const { id, name, phone, email } = req.body;
 
-  contacts.push(contact);
+  const newContact = {
+    id,
+    name,
+    phone,
+    email,
+  };
+  contacts.push(newContact);
 
   return res.json(contacts);
 });
 
-server.put("/contacts", (req, res) => {
-  const contact = req.body;
-  if (contacts.indexOf(contact.id)) contacts.push(contact);
-  else console.log("Contact not exist");
+server.put("/contacts/:id", (req, res) => {
+  const { id } = req.params;
+
+  const { name, phone, email } = req.body;
+
+  const contactIndex = contacts.findIndex((contact) => contact.id === id);
+  contacts[contactIndex] = {
+    ...contacts[contactIndex],
+    name,
+    phone,
+    email,
+  };
+
   return res.json(contacts);
 });
 
-server.delete(`/contacts/`, (req, res) => {
-  const { index } = req.body;
+server.delete("/contacts/:id", (req, res) => {
+  const { id } = req.params;
+  const contactIndex = contacts.findIndex((contact) => contact.id === id);
+  contacts.splice(contactIndex, 1);
 
-  contacts.splice(index, 1);
-
-  return res.json({ message: "O contato foi deletado" }, contacts);
+  return res.json("O contato foi deletado", contacts);
 });
 
 server.listen(5000);
